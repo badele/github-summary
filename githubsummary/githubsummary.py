d935c715376fa92db42f2b570b9a159cd3fee8e5
badele <bruno.adele@jesuislibre.org>
2013-12-15 19:50:59 +0100

    debug openJson in travis

--- githubsummary/githubsummary.py
+++ githubsummary/githubsummary.py
@@ -109,10 +109,12 @@ def saveto(filename, content):
 
 def openJSONFile(filename):
     jsonfile = os.path.abspath(filename)
+    print jsonfile
     if not os.path.isfile(jsonfile):
         return None
 
     json_data = open(jsonfile)
+    print json_data
     return json.load(json_data)
 
 
